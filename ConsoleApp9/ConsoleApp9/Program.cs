61ba4ffc486cabf5aed0af6add100fb6db1cb689
vova836 <123488703+vova836@users.noreply.github.com>
2024-05-16 11:02:38 +0300

    Add files via upload

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaxiDispatcher dispatcher = new TaxiDispatcher();
+            TaxiDriver driver = new TaxiDriver();
+            dispatcher.ProcessTaxiOrder();
+            driver.ProcessDriveTaxi();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp9/ConsoleApp9/TaxiDispatcher.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp9
+{
+    /// <summary>
+    /// Создаем класс Диспетчер такси
+    /// </summary>
+    class TaxiDispatcher
+    {
+        /// <summary>
+        /// Этот метод отвечает за обработку процесса принятия заказа на такси от пользователя.
+        /// </summary>
+        public void ProcessTaxiOrder()
+        {
+            /// <summary>
+            /// Взаимодействие Пользователя и Диспетчера через конслоль, слова Пользователя
+            /// </summary>
+            Console.Write("Пользователь: ");
+            string talktion = Console.ReadLine();
+
+            /// <summary>
+            /// Взаимодействие Пользователя и Диспетчера через конслоль, слова Диспетчера
+            /// </summary>
+            Console.WriteLine("Диспетчер: Здравствуйте, укажите пожалуйста, адрес откуда вас нужно забрать.");
+
+            /// <summary>
+            /// Взаимодействие Пользователя и Диспетчера через конслоль, слова Пользователя
+            /// </summary>
+            Console.Write("Пользователь: ");
+            string pickupLocation = Console.ReadLine();
+
+            /// <summary>
+            /// Взаимодействие Пользователя и Диспетчера через конслоль, слова Диспетчера
+            /// </summary>
+            Console.WriteLine("Диспетчер: И куда вас нужно отвезти?");
+
+            /// <summary>
+            /// Взаимодействие Пользователя и Диспетчера через конслоль, слова Пользователя
+            /// </summary>
+            Console.Write("Пользователь: ");
+            string destination = Console.ReadLine();
+
+            /// <summary>
+            /// Взаимодействие Пользователя и Диспетчера через конслоль, слова Диспетчера
+            /// </summary>
+            Console.WriteLine("Диспетчер: Приблизительное время подачи такси?");
+
+            /// <summary>
+            /// Взаимодействие Пользователя и Диспетчера через конслоль, слова Пользователя
+            /// </summary>
+            Console.Write("Пользователь: ");
+            string pickupTime = Console.ReadLine();
+
+            /// <summary>
+            /// Взаимодействие Пользователя и Диспетчера через конслоль, слова Диспетчера
+            /// </summary>
+            Console.WriteLine("Диспетчер: Хорошо, к вам будет отправлено свободное такси.");
+            Console.WriteLine("Диспетчер: Вы получите уведомление на телефон, когда машина будет назначена.");
+
+            /// <summary>
+            /// Создаем объект такси, который хранит номер машины, цвет машины и имя водителя
+            /// </summary>
+            Taxi taxi = new Taxi
+            {
+                CarNumber = "789-CD-789",
+                Color = "белый",
+                DriverName = "Петров Петр Петрович"
+            };
+
+            /// <summary>
+            /// Задержка потока которая приостанавливает выполнения программы на 2 секунды
+            /// </summary>
+            Thread.Sleep(2000);
+            /// <summary>
+            /// Появляется Уведомление
+            /// </summary>
+            Console.WriteLine($"Уведомление: Вам назначено такси. Номер: {taxi.CarNumber}, Цвет: {taxi.Color}. Водитель: {taxi.DriverName}.");
+        }
+    }
+}
