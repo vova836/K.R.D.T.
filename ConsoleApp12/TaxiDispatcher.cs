125772c6b65364ed847fb1a24d243693a18f29c6
vova836 <123488703+vova836@users.noreply.github.com>
2024-05-23 16:01:13 +0300

    Add files via upload

---
+++ ConsoleApp12/TaxiDispatcher.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp12
+{
+    class TaxiDispatcher
+    {
+        private string[] dispatcherMetting = {
+        "Здравствуйте, укажите пожалуйста, адрес откуда вас нужно забрать.",
+        "Здраствуйте! Пожалуйста, укажите свое место назначения.",
+        "Здраствуйте, укажите пожалуйста ваш адрес.",
+        "Здраствуйте, пожалуйста укажите где вы сейчас находитесь."
+    };
+        private string[] dispatcherGoing = {
+        "И куда вас нужно отвезти?",
+        "Теперь скажите, куда вы хотите ехать?",
+        "Скажите, пожалуйста, куда вам надо ехать?",
+        "Куда вам нужно направиться сегодня?"
+    };
+        private string[] dispatcherTimadialog = {
+        "Приблизительное время подачи такси?",
+        "Пожалуйста укажите время подачи такси?",
+        "Через сколько минут или часов вы хотите чтобы к вам подъехала такси?",
+        "Через сколько времени к вам должно подъехать такси?"
+    };
+        private string[] dispatcherTaxidiases = {
+        "Хорошо! Ваш заказ принят, к вам приедет свободное такси",
+        "Хорошо, к вам будет отправлено свободное такси.",
+        "Принято к вам скоро приедет такси",
+        "Понял, к вам скоро будет отправлен такси"
+    };
+        private string[] inappropriateResponses = {
+        "Пожалуйста, не используйте ненормативную лексику. Мы хотим вам помочь.",
+        "Попрошу вас говорить культурно. Мы здесь, чтобы помочь вам.",
+        "Давайте общаться без грубостей. Чтобы вы смоги нормально поехать.",
+        "Пожалуйста ведите себя прилично."
+    };
+
+        public void ProcessTaxiOrder()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Thread.Sleep(1000);
+            Console.Write("Пользователь: ");
+            string talktion = Console.ReadLine();
+
+            if (ContainsInappropriateLanguage(talktion))
+            {
+                RespondWithInappropriateLanguageWarning();
+                return;
+            }
+
+            Random rand = new Random();
+            int index = rand.Next(dispatcherMetting.Length);
+            Console.ForegroundColor = ConsoleColor.Black;
+            Thread.Sleep(1000);
+            Console.WriteLine("Диспетчер: " + dispatcherMetting[index]);
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Thread.Sleep(1000);
+            Console.Write("Пользователь: ");
+            string pickupLocation = Console.ReadLine();
+
+            if (ContainsInappropriateLanguage(pickupLocation))
+            {
+                RespondWithInappropriateLanguageWarning();
+                return;
+            }
+
+            index = rand.Next(dispatcherGoing.Length);
+            Console.ForegroundColor = ConsoleColor.Black;
+            Thread.Sleep(1000);
+            Console.WriteLine("Диспетчер: " + dispatcherGoing[index]);
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Thread.Sleep(1000);
+            Console.Write("Пользователь: ");
+            string destination = Console.ReadLine();
+
+            if (ContainsInappropriateLanguage(destination))
+            {
+                RespondWithInappropriateLanguageWarning();
+                return;
+            }
+
+            index = rand.Next(dispatcherTimadialog.Length);
+            Console.ForegroundColor = ConsoleColor.Black;
+            Thread.Sleep(1000);
+            Console.WriteLine("Диспетчер: " + dispatcherTimadialog[index]);
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Thread.Sleep(1000);
+            Console.Write("Пользователь: ");
+            string pickupTime = Console.ReadLine();
+
+            if (ContainsInappropriateLanguage(pickupTime))
+            {
+                RespondWithInappropriateLanguageWarning();
+                return;
+            }
+
+
+            index = rand.Next(dispatcherTaxidiases.Length);
+            Console.ForegroundColor = ConsoleColor.Black;
+            Thread.Sleep(1000);
+            Console.WriteLine("Диспетчер: " + dispatcherTaxidiases[index]);
+            Console.ForegroundColor = ConsoleColor.Black;
+            Thread.Sleep(1000);
+            Console.WriteLine("Диспетчер: Также вы получите уведомление на телефон, какая машина к вам приедет и кто водитель.");
+            
+            Taxi taxi = new Taxi
+            {
+                CarNumber = "789-CD-789",
+                Color = "белый",
+                DriverName = "Петров Петр Петрович"
+            };
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Thread.Sleep(3000);
+            Console.WriteLine($"Уведомление: Вам назначено такси.");
+            Console.WriteLine($"Уведомление: Номер машины {taxi.CarNumber}");
+            Console.WriteLine($"Уведомление: Цвет машины {taxi.Color}");
+            Console.WriteLine($"Уведомление: Водитель такси {taxi.DriverName} ");
+        }
+
+        private bool ContainsInappropriateLanguage(string input)
+        {
+            string[] inappropriateWords = { "блядь", "сука", "нахуй", "хуй", "ебать", "пиздец" }; // Add more inappropriate words as needed
+            foreach (string word in inappropriateWords)
+            {
+                if (Regex.IsMatch(input, @"\b" + Regex.Escape(word) + @"\b", RegexOptions.IgnoreCase))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void RespondWithInappropriateLanguageWarning()
+        {
+            Random rand = new Random();
+            int index = rand.Next(inappropriateResponses.Length);
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Диспетчер: " + inappropriateResponses[index]);
+        }
+    }
+}
+
+
---
+++ ConsoleApp12/TaxiDriver.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp12
+{
+    class TaxiDriver
+    {
+        private string[] driverMetting = {
+        "Здравствуйте, диспетчер сообщил мне куда нам надо ехать. Готовы ехать?",
+        "Добрый день, мне диспетчер сообщил куда вам нужно ехать. Можем мы начать поездку?",
+        "Здравствуйте, у меня уже есть информация о вашем пункте назначения. Начинаем выдвигаться?",
+        "Здрастье, диспетчер указал мне информацию куда нам нужна ехать. Выдвигаемся?"
+    };
+
+        private string[] driverQuestion = {
+        "Хотите музыку или радио?",
+        "Включить вам радио или музыку?",
+        "Вам включить радио или музыку?",
+        "Радио или музыку вам включить?"
+    };
+
+        public void ProcessDriveTaxi()
+        {
+            Random rand = new Random();
+            int index = rand.Next(driverMetting.Length);
+            Console.ForegroundColor = ConsoleColor.Black;
+            Thread.Sleep(1000);
+            Console.WriteLine("Водитель: " + driverMetting[index]);
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Thread.Sleep(1000);
+            Console.Write("Пользователь: ");
+            string pickupLocation = Console.ReadLine();
+
+            index = rand.Next(driverQuestion.Length);
+            Console.ForegroundColor = ConsoleColor.Black;
+            Thread.Sleep(1000);
+            Console.WriteLine("Водитель: " + driverQuestion[index]);
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Thread.Sleep(1000);
+            Console.Write("Пользователь: ");
+            string destination = Console.ReadLine();
+
+            // Проверка на наличие слова "нет" в ответе пользователя
+            if (!destination.ToLower().Contains("нет"))
+            {
+                if (destination.ToLower().Contains("музыка"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Водитель: Хорошо я включу вам музыку.");
+                    // Здесь можно добавить код для включения музыки
+                }
+                else if (destination.ToLower().Contains("музыку"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Водитель: Хорошо я включу вам музыку.");
+                }
+                else if (destination.ToLower().Contains("радио"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Водитель: Хорошо я включу вам радио.");
+                    // Здесь можно добавить код для включения радио
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Thread.Sleep(1000);
+                Console.WriteLine("Водитель: Хорошо, как скажите вы решаете.");
+            }
+        }
+    }
+}
